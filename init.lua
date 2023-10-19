712630731d11b1166954ec1c571f78ea7b7aa05b
396458015 <396458015@qq.com>
2023-10-19 11:55:03 +0800

    update leaderf configuration

--- init.lua
+++ init.lua
@@ -615,21 +615,11 @@ require("lazy").setup({
     vim.g.Lf_IgnoreCurrentBufferName = 1
     vim.cmd([[source C:/Users/ThinkPad/AppData/Local/nvim-data/Maxl/Local_Plugins/leaderf_popupColorscheme_nightfox.vim]])  -- PopupColorscheme
 	vim.g.Lf_WorkingDirectoryMode = 'Ac'
-	vim.g.Lf_DefaultMode          = 'NameOnly'
-    vim.g.Lf_WindowPosition       = 'popup'
-    vim.g.Lf_WindowHeight         = 0.4
-    vim.g.Lf_PopupHeight          = 0.4
-    vim.g.Lf_PopupWidth           = 0.8
-    vim.g.Lf_PopupPosition        = { 1, 0 }
-    vim.g.Lf_PopupPreviewPosition = 'bottom'
-    vim.g.Lf_PreviewPosition      = 'center'
-	vim.g.Lf_PopupShowStatusline  = 0
-	vim.g.Lf_PopupShowBorder      = 1
-    vim.g.Lf_JumpToExistingWindow = 0
-	vim.g.Lf_PopupBorders         = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' }
-    vim.g.Lf_StlSeparator = { left = "", right = "" }
-    vim.g.Lf_PreviewCode    = 1
+	vim.g.Lf_DefaultMode = 'NameOnly'
+    vim.g.Lf_PreviewCode = 1
     vim.g.Lf_PreviewInPopup = 1
+    vim.g.Lf_StlSeparator = { left = "", right = "" }
+    vim.g.Lf_JumpToExistingWindow = 0
     vim.g.Lf_PreviewResult = {
         File = 1,
         Buffer = 0,
@@ -639,21 +629,54 @@ require("lazy").setup({
         Function = 1,
         Line = 1,
         Colorscheme = 0,
-        Rg = 0,
+        Rg = 1,
         Gtags = 0,
     }-- 0:不自动预览; 1:自动预览
     vim.g.Lf_NeedCacheTime = 0.1  -- cache the files list,if time > 0.1s.
     vim.g.Lf_CacheDirectory = "C:/Users/ThinkPad/AppData/Local/nvim-data/Maxl/cache/leaderf_mru_cache"
-    vim.g.Lf_MruMaxFiles = 2048 -- Specify the number of most recently used files you want LeaderF to record (default: 100).
+    vim.g.Lf_MruMaxFiles = 2048
     vim.g.Lf_MruEnableFrecency = 0
     vim.g.Lf_ShowRelativePath = 0
     vim.g.Lf_WildIgnore = {
       dir = { ".svn", ".git", ".hg" },
       file = { "*.sw?", "~$*", "*.bak", "*.exe", "*.o", "*.so", "*.py[co]" },
-    },
-    -- 使用:LeaderfRg 路径不全, 搜索该录下经的文件.
+    }
+    -- 使用:LeaderfRg 路径不全时, 默认搜索该文件目录下的文件
     vim.cmd([[command! -bar -nargs=? -complete=dir LeaderfRg Leaderf! rg "" <q-args>]])
+
+    -- Bottom mode & Change statusline color (not popup mode)
+    vim.g.Lf_WindowPosition = 'bottom'
+    vim.api.nvim_command("hi link Lf_hl_stlName StatuslineNC")
+    vim.api.nvim_command("hi link Lf_hl_stlMode StatuslineNC")
+    vim.api.nvim_command("hi link Lf_hl_stlCategory StatuslineNC")
+    vim.api.nvim_command("hi link Lf_hl_stlSeparator0 StatuslineNC")
+    vim.api.nvim_command("hi link Lf_hl_stlSeparator1 StatuslineNC")
+    vim.api.nvim_command("hi link Lf_hl_stlSeparator2 StatuslineNC")
+    vim.api.nvim_command("hi link Lf_hl_stlSeparator3 StatuslineNC")
+    vim.api.nvim_command("hi link Lf_hl_stlSeparator4 StatuslineNC")
+    vim.api.nvim_command("hi link Lf_hl_stlSeparator5 StatuslineNC")
+    vim.api.nvim_command("hi link Lf_hl_stlLineInfo Statusline")
+    vim.api.nvim_command("hi link Lf_hl_stlNameOnlyMode StatuslineNC")
+    vim.api.nvim_command("hi link Lf_hl_stlRegexMode StatuslineNC")
+    vim.api.nvim_command("hi link Lf_hl_stlFullPathMode StatuslineNC")
+    vim.api.nvim_command("hi link Lf_hl_stlFuzzyMode StatuslineNC")
+    vim.api.nvim_command("hi link Lf_hl_stlCwd Statusline")
+    vim.api.nvim_command("hi link Lf_hl_stlBlank Statusline")
+    vim.api.nvim_command("hi link Lf_hl_stlTotal Statusline")
+
+    -- Popup mode
+    --[[ vim.g.Lf_WindowPosition       = 'popup'
+    vim.g.Lf_WindowHeight         = 0.4
+    vim.g.Lf_PopupHeight          = 0.4
+    vim.g.Lf_PopupWidth           = 0.8
+    vim.g.Lf_PopupPosition        = { 1, 0 }
+    vim.g.Lf_PopupPreviewPosition = 'bottom'
+    vim.g.Lf_PreviewPosition      = 'center'
+	vim.g.Lf_PopupShowStatusline  = 0
+	vim.g.Lf_PopupShowBorder      = 1
+	vim.g.Lf_PopupBorders         = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' } ]]
     end,
+
     init = function()
     neomap("n", "<localleader>fb", ":LeaderfFile :/<left><left>", key_opts_n)
     neomap("n", "<localleader>fp", ":Leaderf rg<CR>", key_opts_ns)
@@ -2668,11 +2691,12 @@ augroup END
 
 -- {{{ colorscheme
 random_color = {
+--light
     'catppuccin-latte',
     -- 'tokyonight-day',
     -- 'github_light',
     -- 'dayfox',
-
+--dark
     -- 'duskfox',
     -- 'terafox',
     -- 'nordfox',
