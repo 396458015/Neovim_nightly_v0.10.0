93291df9878d6551acd29ac64119ea4c17680cd0
396458015 <396458015@qq.com>
2023-12-05 19:40:56 +0800

    update

--- init.lua
+++ init.lua
@@ -787,11 +787,15 @@ require("lazy").setup({
             vim.g.floaterm_keymap_next = '<leader>tn'
             vim.g.floaterm_shell="C:/PROGRA~1/PowerShell/7/pwsh.exe"
             neomap("n","<leader>to",":FloatermNew --position=center --width=0.9 --height=0.9<CR>")
-            -- neomap("n","<leader>lf",":FloatermNew! --position=center --width=0.9 --height=0.9 lfcd<CR>") --slower
-            neomap("n","<leader>lf",":FloatermNew --position=center --width=0.9 --height=0.9 lfcd<CR>") --faster
+            -- neomap("n","<localleader>e",":FloatermNew! --position=center --width=0.9 --height=0.9 --autoclose=1 lfcd<CR>") --slower
+            neomap("n","<localleader>e",":FloatermNew --position=center --width=0.9 --height=0.9 --autoclose=1 lfcd<CR>") --faster
             neomap("n","<leader>tt",":FloatermToggle<CR>")
             neomap("n","<leader>tr",":FloatermNew<CR>rg.exe<Space>")
             neomap('n', '<M-o>', ':FloatermNew SumatraPdf <C-r><C-l><CR>', {})
+
+            neomap("n","<leader>tj",":FloatermNext<CR>")
+            neomap("n","<leader>tk",":FloatermPrev<CR>")
+
             vim.cmd[[
             augroup Compiler_code
                 au!
