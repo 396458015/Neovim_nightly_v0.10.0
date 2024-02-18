b52080a44a8210385a422cb11e35a73c014d6c28
396458015 <396458015@qq.com>
2024-02-18 21:51:06 +0800

    add lualine custom theme file

---
+++ lua/lualine_custom_theme.lua
@@ -0,0 +1,164 @@
+local max_lualine_theme_frappe_transparent = require'lualine.themes.palenight'
+max_lualine_theme_frappe_transparent = {
+    normal = {
+        a = { fg = '#303446', bg = '#89a0c3', gui = 'bold' },
+        b = { fg = '#89a0c3', bg = nil },
+        c = { fg = '#71839b', bg = nil },
+    },
+    insert = {
+        a = { fg = '#303446', bg = '#a7e32a', gui = 'bold' },
+        b = { fg = '#a7e32a', bg = nil },
+    },
+    visual = {
+        a = { fg = '#303446', bg = '#ae81ff', gui = 'bold' },
+        b = { fg = '#ae81ff', bg = nil },
+    },
+    replace = {
+        a = { fg = '#303446', bg = '#C3E88D', gui = 'bold' },
+        b = { fg = '#C3E88D', bg = '#303446' },
+    },
+    inactive = {
+        a = { fg = '#303446', bg = '#303446', gui = 'bold' },
+        b = { fg = '#303446', bg = '#303446' },
+        c = { fg = '#303446', bg = '#303446' },
+    }
+}
+
+local max_lualine_theme_latte_transparent = require'lualine.themes.palenight'
+max_lualine_theme_latte_transparent = {
+    normal = {
+        a = { fg = '#1e66f5', bg = nil, gui = 'bold' },
+        b = { fg = '#1e66f5', bg = nil },
+        c = { fg = '#71839b', bg = nil },
+    },
+    insert = {
+        a = { fg = '#2c8a5e', bg = nil, gui = 'bold' },
+        b = { fg = '#2c8a5e', bg = nil },
+    },
+    visual = {
+        a = { fg = '#925ba6', bg = nil, gui = 'bold' },
+        b = { fg = '#925ba6', bg = nil },
+    },
+    replace = {
+        a = { fg = '#e1e2e7', bg = '#C3E88D', gui = 'bold' },
+        b = { fg = '#C3E88D', bg = '#e1e2e7' },
+    },
+    inactive = {
+        a = { fg = '#e1e2e7', bg = '#e1e2e7', gui = 'bold' },
+        b = { fg = '#e1e2e7', bg = '#e1e2e7' },
+        c = { fg = '#e1e2e7', bg = '#e1e2e7' },
+    }
+}
+
+local max_lualine_theme_frappe = require'lualine.themes.palenight'
+max_lualine_theme_frappe = {
+    normal = {
+        a = { fg = '#303446', bg = '#89a0c3', gui = 'bold' },
+        b = { fg = '#89a0c3', bg = '#303446' },
+        c = { fg = '#71839b', bg = '#303446' },
+    },
+    insert = {
+        a = { fg = '#303446', bg = '#a7e32a', gui = 'bold' },
+        b = { fg = '#a7e32a', bg = '#303446' },
+    },
+    visual = {
+        a = { fg = '#303446', bg = '#ae81ff', gui = 'bold' },
+        b = { fg = '#ae81ff', bg = '#303446' },
+    },
+    replace = {
+        a = { fg = '#303446', bg = '#C3E88D', gui = 'bold' },
+        b = { fg = '#C3E88D', bg = '#303446' },
+    },
+    inactive = {
+        a = { fg = '#303446', bg = '#303446', gui = 'bold' },
+        b = { fg = '#303446', bg = '#303446' },
+        c = { fg = '#303446', bg = '#303446' },
+    }
+}
+
+local max_lualine_theme_latte = require'lualine.themes.palenight'
+max_lualine_theme_latte = {
+    normal = {
+        a = { fg = '#1e66f5', bg = nil, gui = 'bold' },
+        b = { fg = '#1e66f5', bg = nil },
+        c = { fg = '#71839b', bg = nil },
+    },
+    insert = {
+        a = { fg = '#2c8a5e', bg = nil, gui = 'bold' },
+        b = { fg = '#2c8a5e', bg = nil },
+    },
+    visual = {
+        a = { fg = '#925ba6', bg = nil, gui = 'bold' },
+        b = { fg = '#925ba6', bg = nil },
+    },
+    replace = {
+        a = { fg = '#e1e2e7', bg = '#C3E88D', gui = 'bold' },
+        b = { fg = '#C3E88D', bg = '#e1e2e7' },
+    },
+    inactive = {
+        a = { fg = '#e1e2e7', bg = '#e1e2e7', gui = 'bold' },
+        b = { fg = '#e1e2e7', bg = '#e1e2e7' },
+        c = { fg = '#e1e2e7', bg = '#e1e2e7' },
+    }
+}
+
+local max_lualine_theme_macchiato = require'lualine.themes.palenight'
+max_lualine_theme_macchiato = {
+    normal = {
+        a = { fg = '#24273a', bg = '#89a0c3', gui = 'bold' },
+        b = { fg = '#89a0c3', bg = '#24273a' },
+        c = { fg = '#71839b', bg = '#24273a' },
+    },
+    insert = {
+        a = { fg = '#24273a', bg = '#a7e32a', gui = 'bold' },
+        b = { fg = '#a7e32a', bg = '#24273a' },
+    },
+    visual = {
+        a = { fg = '#24273a', bg = '#ae81ff', gui = 'bold' },
+        b = { fg = '#ae81ff', bg = '#24273a' },
+    },
+    replace = {
+        a = { fg = '#24273a', bg = '#C3E88D', gui = 'bold' },
+        b = { fg = '#C3E88D', bg = '#24273a' },
+    },
+    inactive = {
+        a = { fg = '#24273a', bg = '#24273a', gui = 'bold' },
+        b = { fg = '#24273a', bg = '#24273a' },
+        c = { fg = '#24273a', bg = '#24273a' },
+    }
+}
+
+local max_lualine_theme_mocha = require'lualine.themes.palenight'
+max_lualine_theme_mocha = {
+    normal = {
+        a = { fg = '#352939', bg = '#89a0c3', gui = 'bold' },
+        b = { fg = '#89a0c3', bg = '#352939' },
+        c = { fg = '#71839b', bg = '#352939' },
+    },
+    insert = {
+        a = { fg = '#352939', bg = '#a7e32a', gui = 'bold' },
+        b = { fg = '#a7e32a', bg = '#352939' },
+    },
+    visual = {
+        a = { fg = '#352939', bg = '#ae81ff', gui = 'bold' },
+        b = { fg = '#ae81ff', bg = '#352939' },
+    },
+    replace = {
+        a = { fg = '#352939', bg = '#C3E88D', gui = 'bold' },
+        b = { fg = '#C3E88D', bg = '#352939' },
+    },
+    inactive = {
+        a = { fg = '#352939', bg = '#352939', gui = 'bold' },
+        b = { fg = '#352939', bg = '#352939' },
+        c = { fg = '#352939', bg = '#352939' },
+    }
+}
+
+return {
+    max_lualine_theme_frappe_transparent = max_lualine_theme_frappe_transparent,
+    max_lualine_theme_latte_transparent  = max_lualine_theme_latte_transparent,
+    max_lualine_theme_frappe             = max_lualine_theme_frappe,
+    max_lualine_theme_latte              = max_lualine_theme_latte,
+    max_lualine_theme_macchiato          = max_lualine_theme_macchiato,
+    max_lualine_theme_mocha              = max_lualine_theme_mocha,
+}
